fcc065f88c6ad1a5270364b9b5a7554432a16aae
Danda Keerthi <dandakeerthi2@gmail.com>
2026-03-14 09:08:36 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,51 +1,91 @@
 import streamlit as st
-from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import CharacterTextSplitter
+import pdfplumber 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains.retrieval_qa.base import RetrievalQA
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
+st.header("RAG-Chatbot")
+with st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader("Upload a PDF file and start asking questions",type="pdf")
 
-st.title("RAG Chatbot 🤖")
+# get user question
+user_question=st.text_input("Type your question here")
 
-# Load document
-loader = TextLoader("data/sample.txt")
-documents = loader.load()
+# Extract the pdf and chunk it
+if file is not None:
+    # extract test from it
+    
+    with pdfplumber.open(file) as pdf:
+        text=""
+        for page in pdf.pages:
+            text+=page.extract_text() + "\n"
+    # st.write(text)
 
-# Split document
-text_splitter = CharacterTextSplitter(
-    chunk_size=500,
-    chunk_overlap=50
-)
+    # split data into chunks
+    text_splitter=RecursiveCharacterTextSplitter(
+        separators=["\n\n","\n","."," ",""],
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+    chunks=text_splitter.split_text(text)
+    # st.write(chunks)
 
-docs = text_splitter.split_documents(documents)
+    # Generate Embeddings
 
-# Create embeddings
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
 
-# Create vector database
-vectorstore = FAISS.from_documents(docs, embeddings)
 
-# Retriever
-retriever = vectorstore.as_retriever()
+    # store embeddings in vector db
+    vector_store = FAISS.from_texts(chunks, embeddings)
 
-# LLM
-llm = Ollama(model="tinyllama")
+    
 
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    retriever=retriever
-)
+    # generate answer
+    # question -> embeddings -> similarity search -> results to llm -> response
+    prompt = ChatPromptTemplate.from_messages([
+        ("system",
+         """You are a helpful assistant answering questions about a PDF document.
+         Guidelines:
+         1. Provide complete, well-explained answers using the context below.
+         2. Include relevant details, numbers, and explanations.
+         3. If related information exists in context, include it.
+         4. Only use information from the provided context.
+         5. Summarize long answers when needed.
+         6. If answer is not in the context, say you don't know politely.
+         Context: \n{context}"""),
+         ("human", "{question}")
+         ])
 
-# User input
-query = st.text_input("Ask something")
+    # LLM
+    llm = Ollama(model="tinyllama")
 
-if query:
-    result = qa_chain.run(query)
-    st.write(result)
+
+    # Define format function
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+    retriever = vector_store.as_retriever(
+    search_type="mmr",
+    search_kwargs={"k": 4}
+    )
+
+    chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+    )
+    if user_question:
+        with st.spinner("Generating answer..."):
+            response=chain.invoke(user_question)
+            st.write(response)
+
+
